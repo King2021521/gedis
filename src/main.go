22a3bfd589028f151aeb3df0658bec6898a58767
zxm <zhangxiaomin@viewhigh.com>
2019-11-08 11:17:31 +0800

    1、增加其他string相关的模板方法 2、相关代码逻辑优化、增加校验等等

--- src/main.go
+++ src/main.go
@@ -11,34 +11,10 @@ import (
 )
 
 func main() {
-	//testRange(protocol.SET,"name","zxm")
-	//testPool(getConn())
-	//testMset(getConn())
 	conn:=getConn()
-	template.Set("aaa","aaa",conn)
-	result:=template.Del("aaa",conn)
-	fmt.Println(result)
-}
+	result,_:=template.Mset(conn,"root111","1111","asdasdad","asdadad")
 
-func testRange(cmd string, a ...interface{}){
-	var buffer bytes.Buffer
-	buffer.WriteString(protocol.ASTERISKBYTE)
-	buffer.WriteString(strconv.Itoa(len(a)+1))
-	buffer.WriteString(protocol.CRLF)
-	buffer.WriteString(protocol.DOLLARBYTE)
-	buffer.WriteString(strconv.Itoa(len(cmd)))
-	buffer.WriteString(protocol.CRLF)
-	buffer.WriteString(cmd)
-	buffer.WriteString(protocol.CRLF)
-
-	for _, arg := range a {
-		buffer.WriteString(protocol.DOLLARBYTE)
-		buffer.WriteString(strconv.Itoa(len(arg.(string))))
-		buffer.WriteString(protocol.CRLF)
-		buffer.WriteString(arg.(string))
-		buffer.WriteString(protocol.CRLF)
-	}
-	fmt.Println(buffer.String())
+	fmt.Println(result,"111")
 }
 
 func getConn() *net.TCPConn{
@@ -50,61 +26,4 @@ func getConn() *net.TCPConn{
 
 	conn,_:=tcp.GetConn(pool)
 	return conn
-}
-
-func testMset(conn *net.TCPConn){
-	r1:=template.Mset(conn,"lakers","james","sun","book","cc","$1")
-	fmt.Println(r1)
-	r2:=template.Mget(conn,"lakers","sun1","cc")
-	fmt.Println("结果",r2,"len",len(r2),"cap",cap(r2))
-}
-
-func testPool(conn *net.TCPConn){
-	sendResult1 := template.Set("country1", "China LiaoNing DaLian", conn)
-	sendResult11 := template.Expire("country1", 10000, conn)
-	fmt.Println("send result1:" , sendResult1)
-	fmt.Println("send result11:" , sendResult11)
-	result := template.Get("country1", conn)
-	fmt.Println("get result country1:" + result)
-
-	/*pool.PutConn(conn)
-
-	conn1,_:=tcp.GetConn(pool)
-	fmt.Println(conn1.RemoteAddr())
-	sendResult1 := template.Set("name", "james", conn)
-	fmt.Println("send result:" + sendResult1)
-	result1 := template.Get("name", conn)
-	fmt.Println("get result:" + result1)*/
-	/*conn1,_:=tcp.GetConn(pool)
-	fmt.Println(conn1.RemoteAddr())
-
-	conn2,_:=tcp.GetConn(pool)
-	fmt.Println(conn2.RemoteAddr())
-
-	conn3,_:=tcp.GetConn(pool)
-	fmt.Println(conn3.RemoteAddr())
-
-	_, err = tcp.GetConn(pool)
-	fmt.Println(err)
-
-	size:=tcp.PoolSize(pool)
-	fmt.Println("连接数",size)
-	if size<1{
-		pool.PutConn(conn3)
-	}
-	size1:=tcp.PoolSize(pool)
-	fmt.Println("连接数",size1)
-
-	conn4,_:=tcp.GetConn(pool)
-	fmt.Println(conn4.RemoteAddr())*/
-}
-
-func testRedis(){
-	conn := tcp.Connect("10.10.5.239:6379")
-	authResult := template.Auth("123456", conn)
-	fmt.Println("auth result:" + authResult)
-	sendResult := template.Set("name", "james", conn)
-	fmt.Println("send result:" + sendResult)
-	result := template.Get("name", conn)
-	fmt.Println("get result:" + result)
 }
--- src/template/gedis_template.go
+++ src/template/gedis_template.go
@@ -14,30 +14,50 @@ import (
 /**
  * 认证权限
  */
-func Auth(pwd string, conn *net.TCPConn) string {
-	return sendCommand(conn, protocol.AUTH, SafeEncode(pwd))
+func Auth(pwd string, conn *net.TCPConn) (interface{}, error) {
+	result := sendCommand(conn, protocol.AUTH, SafeEncode(pwd))
+	if result != protocol.OK {
+		return nil, fmt.Errorf(result)
+	}
+	return result, nil
 }
 
-func Set(key string, value string, conn *net.TCPConn) string {
-	return sendCommand(conn, protocol.SET, SafeEncode(key), SafeEncode(value))
+func Set(key string, value string, conn *net.TCPConn) (interface{}, error) {
+	result := sendCommand(conn, protocol.SET, SafeEncode(key), SafeEncode(value))
+	if result != protocol.OK {
+		return nil, fmt.Errorf(result)
+	}
+	return strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), nil
 }
 
-func Get(key string, conn *net.TCPConn) string {
+func Get(key string, conn *net.TCPConn) (interface{}, error) {
 	result := sendCommand(conn, protocol.GET, SafeEncode(key))
+	if strings.HasPrefix(result, protocol.NONEXIST) {
+		return nil, nil
+	}
+
+	if !strings.HasPrefix(result, protocol.DOLLARBYTE) {
+		return nil, fmt.Errorf(result)
+	}
 	array := strings.Split(result, protocol.CRLF)
-	return array[1]
+	return array[1], nil
 }
 
-func Expire(key string, value int64, conn *net.TCPConn) string {
-	return sendCommand(conn, protocol.EXPIRE, SafeEncode(key), encodeInt(value))
+func Expire(key string, value int64, conn *net.TCPConn) interface{} {
+	result := sendCommand(conn, protocol.EXPIRE, SafeEncode(key), encodeInt(value))
+	return strings.ReplaceAll(strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), protocol.COLON_BYTE, protocol.BLANK)
 }
 
-func Mset(conn *net.TCPConn, keyvalues ...string) string {
+func Mset(conn *net.TCPConn, keyvalues ...string) (interface{}, error) {
 	bytes := make([][]byte, len(keyvalues))
 	for i := 0; i < len(keyvalues); i++ {
 		bytes[i] = SafeEncode(keyvalues[i])
 	}
-	return sendCommand(conn, protocol.MSET, bytes...)
+	result := sendCommand(conn, protocol.MSET, bytes...)
+	if result != protocol.OK {
+		return nil, fmt.Errorf(result)
+	}
+	return strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), nil
 }
 
 func Mget(conn *net.TCPConn, keys ...string) []interface{} {
@@ -49,7 +69,7 @@ func Mget(conn *net.TCPConn, keys ...string) []interface{} {
 	array := strings.Split(result, protocol.CRLF)
 	results := arraylist.New()
 	for i := 1; i < len(array)-1; i++ {
-		if array[i] == "$-1" {
+		if array[i] == protocol.NONEXIST {
 			results.Add(nil)
 			continue
 		}
@@ -62,9 +82,40 @@ func Mget(conn *net.TCPConn, keys ...string) []interface{} {
 	return results.Values()
 }
 
-func Del(key string, conn *net.TCPConn) string {
+func Del(key string, conn *net.TCPConn) interface{} {
 	result := sendCommand(conn, protocol.DEL, SafeEncode(key))
-	return strings.ReplaceAll(result,protocol.COLON_BYTE,"")
+	return strings.ReplaceAll(strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), protocol.COLON_BYTE, protocol.BLANK)
+}
+
+func Setnx(key string, value string, conn *net.TCPConn) interface{} {
+	return sendCommand(conn, protocol.SETNX, SafeEncode(key), SafeEncode(value))
+}
+
+func Incr(key string, conn *net.TCPConn) interface{} {
+	result := sendCommand(conn, protocol.INCR, SafeEncode(key))
+	return strings.ReplaceAll(
+		strings.ReplaceAll(
+			result, protocol.CRLF, protocol.BLANK),
+		protocol.COLON_BYTE, protocol.BLANK)
+}
+
+func Decr(key string, conn *net.TCPConn) interface{} {
+	result := sendCommand(conn, protocol.DECR, SafeEncode(key))
+	return strings.ReplaceAll(strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), protocol.COLON_BYTE, protocol.BLANK)
+}
+
+func Setex(key string, time int64, value string, conn *net.TCPConn) (interface{}, error) {
+	result := sendCommand(conn, protocol.SETEX, SafeEncode(key), encodeInt(time), SafeEncode(value))
+	if result != protocol.OK {
+		return nil, fmt.Errorf(result)
+	}
+	return strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), nil
+}
+
+func Ttl(key string, conn *net.TCPConn) (int, error) {
+	result := sendCommand(conn, protocol.TTL, SafeEncode(key))
+	ttl := strings.ReplaceAll(strings.ReplaceAll(result, protocol.CRLF, protocol.BLANK), protocol.COLON_BYTE, protocol.BLANK)
+	return strconv.Atoi(ttl)
 }
 
 func sendCommand(conn *net.TCPConn, cmd string, a ...[]byte) string {
@@ -97,18 +148,19 @@ func encodeInt(arg int64) []byte {
 }
 
 func send(conn *net.TCPConn, content bytes.Buffer) string {
-	//给服务器发信息
+	//send to server
 	_, err := conn.Write(content.Bytes())
 
 	if err != nil {
-		fmt.Println(conn.RemoteAddr().String(), "服务器反馈")
+		fmt.Println(conn.RemoteAddr().String(), "server response")
 		os.Exit(1)
 	}
 
 	buffer := make([]byte, 1024)
-	msg, err := conn.Read(buffer) //接受服务器信息
+	//receive server info
+	msg, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println(conn.RemoteAddr().String(), "服务器反馈"+err.Error())
+		fmt.Println(conn.RemoteAddr().String(), "server response:"+err.Error())
 		os.Exit(1)
 	}
 	return string(buffer[:msg])
