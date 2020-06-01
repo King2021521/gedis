4f841a7d05b3ce94995fdd8921d103012d63c093
zhangxiaomin <zhangxiaomin@viewhigh.com>
2020-06-01 10:09:39 +0800

    协议处理模块增加注释

--- src/client/command_executor.go
+++ src/client/command_executor.go
@@ -1,3 +1,4 @@
+//@author zhangxiaomin
 package client
 
 import (
@@ -7,8 +8,12 @@ import (
 	"strconv"
 	"fmt"
 )
-
+/**
+ * 通信协议处理：如redis操作：set key value，解析成redis理解的格式后就是：
+ * *3\r\n$3\r\nset\r\n$3\r\nkey\r\n$5\r\nvalue\r\n
+ */
 func SendCommand(conn *net.TCPConn, cmd string, a ...[]byte) string {
+	//前置处理命令前缀部分，格式固定
 	var buffer bytes.Buffer
 	buffer.Write(protocol.SafeEncode(protocol.ASTERISKBYTE))
 	buffer.Write(protocol.SafeEncode(strconv.Itoa(len(a) + 1)))
@@ -19,6 +24,7 @@ func SendCommand(conn *net.TCPConn, cmd string, a ...[]byte) string {
 	buffer.Write(protocol.SafeEncode(cmd))
 	buffer.Write(protocol.SafeEncode(protocol.CRLF))
 
+	//循环处理命令详情部分（key、value长度不一定固定，所以循环处理，并拼接）
 	for _, arg := range a {
 		buffer.Write(protocol.SafeEncode(protocol.DOLLARBYTE))
 		buffer.Write(protocol.SafeEncode(strconv.Itoa(len(arg))))
@@ -29,6 +35,9 @@ func SendCommand(conn *net.TCPConn, cmd string, a ...[]byte) string {
 	return send(conn, buffer)
 }
 
+/**
+ * tcp通信，请求redis sever
+ */
 func send(conn *net.TCPConn, content bytes.Buffer) string {
 	//send to server
 	_, err := conn.Write(content.Bytes())
